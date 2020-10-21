26dddd2d2d70e4d0f9a383fca64fbb62cf4377b2
Leober Ramos <leober.ramos33@gmail.com>
2020-10-20 23:56:58 -0400

    Change the default view of the activity to the original

--- resources/views/activity/show.blade.php
+++ resources/views/activity/show.blade.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+@section('title', "{{ $course->full_name }} - {{ $activity->subject }}")
 
-    <title>{{ $course->full_name }} - {{ $activity->subject }}</title>
+@section('content')
+<div class="center">
+    <h1>{{ $activity->subject }}</h1>
+    <h5>{{ $user->full_name }}</h5>
+    <h5>{{ $course->full_name }}</h5>
+</div>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
-</head>
-
-<body>
-    <div style="text-align: center">
-        <h1>{{ $activity->subject }}</h1>
-        <h3>{{ $user->full_name }}</h3>
-        <h3>{{ $course->full_name }}</h3>
-    </div>
-
+<div class="browser-default">
     {!! $activity->activity !!}
-</body>
-
-</html>
+</div>
+@endsection
