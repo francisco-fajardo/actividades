9d9fd8e2f1e9744e7ebfef51964705fb8497a3eb
Leober Ramos <leober.ramos33@gmail.com>
2020-12-07 13:00:02 -0400

    Add autoprefixer postcss to Workflow

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,10 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").sass(
-    "resources/sass/app.scss",
-    "public/css"
-);
+mix.js("resources/js/app.js", "public/js/dist")
+    .sass("resources/sass/app.scss", "public/css/dist")
+    .options({
+        postCss: [
+            require("autoprefixer")
+        ]
+    });
