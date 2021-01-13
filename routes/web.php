871a274f2e29f75ee84b7340bb34be14750701ec
Leober Ramos <leober.ramos33@gmail.com>
2021-01-13 15:21:45 -0400

    CoursesController as default view for /

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::pattern("id", "[0-9]+");
-Route::get("/", "HomeController@index")->name("home");
+Route::get("/", "CoursesController@index")->name("home");
 
 /*
 |--------------------------------------------------------------------------
@@ -24,15 +24,6 @@ Route::get("/", "HomeController@index")->name("home");
 Route::get("login", "Auth\LoginController@showLoginForm")->name("login");
 Route::post("login", "Auth\LoginController@login");
 Route::post("logout", "Auth\LoginController@logout")->name("logout");
-
-/*
-|--------------------------------------------------------------------------
-| Courses Routes
-|--------------------------------------------------------------------------
-*/
-Route::prefix("courses")->group(function () {
-    Route::get("/", "CoursesController@index")->name("courses.index");
-});
 
 /*
 |--------------------------------------------------------------------------
