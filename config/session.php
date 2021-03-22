1f9fd9141d0e91c59dcfe1024e464c4565c31ad9
Leober Ramos <leober.ramos33@gmail.com>
2021-03-22 18:57:27 -0400

    Update default config for Session with the env default variables

--- config/session.php
+++ config/session.php
@@ -127,7 +127,7 @@ return [
 
     "cookie" => env(
         "SESSION_COOKIE",
-        Str::slug(env("APP_NAME", "actividades"), "_") . "_session"
+        Str::slug(env("APP_NAME", "Actividades Fajardo"), "_") . "_session"
     ),
 
     /*
