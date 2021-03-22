d0fc2e7efcc639d3284475a6cf08ea47e999648d
Leober Ramos <leober.ramos33@gmail.com>
2021-03-22 19:17:07 -0400

    Move logic of CKEditor embed to a JS file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,8 @@
     <!-- Styles -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" integrity="sha512-UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" crossorigin="anonymous" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" integrity="sha512&#45;UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" crossorigin="anonymous" /> -->
+    <link rel="stylesheet" href="{{ asset('materialize.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/dist/app.css') }}" />
     @yield('styles')
 
@@ -88,7 +89,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" integrity="sha512-NiWqa2rceHnN3Z5j6mSAvbwwg3tiwVNxiAQaaSMSXnRRDh5C2mk/+sKQRw8qjV1vN4nf8iK2a0b048PnHbyx+Q==" crossorigin="anonymous"></script>
     <script src="{{ asset('js/dist/app.js') }}"></script>
-    @yield('endbody')
+    @yield('scripts')
 </body>
 
 </html>
