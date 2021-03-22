328b2212927da01a8502a82766ed170a9020e4ef
yonaikerlol <yonaikerlol@users.noreply.github.com>
2021-03-22 23:43:13 +0000

    Apply formatting changes

--- config/database.php
+++ config/database.php
@@ -123,7 +123,8 @@ return [
             "cluster" => env("REDIS_CLUSTER", "redis"),
             "prefix" => env(
                 "REDIS_PREFIX",
-                Str::slug(env("APP_NAME", "Actividades Fajardo"), "_") . "_database_"
+                Str::slug(env("APP_NAME", "Actividades Fajardo"), "_") .
+                    "_database_"
             ),
         ],
 
